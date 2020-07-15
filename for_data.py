4ee4f8badb74da6b852a09b8ccfad6eefcc73d3c
Your Name <you@example.com>
2020-07-15 17:52:08 +0900

    first commit

---
+++ for_data.py
@@ -0,0 +1,11 @@
+import data_dic
+print(data_dic.elementary_school['학교ID'])
+data_dic.print_ele()
+
+
+# import arg_greet
+# arg_greet.greet()
+
+
+# import factorial_recursion as fr 
+# fr.factorial()
